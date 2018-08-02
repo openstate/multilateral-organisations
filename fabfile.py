f9ddfbb598babcfcdc183275cd785adf403b2e21
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-08-02 19:52:39 +0200

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,8 +5,8 @@ env.hosts = ["Oxygen"]
 
 
 def deploy():
-    with cd('/home/projects/multilaterale-organisaties'):
-        run('git pull git@github.com:openstate/multilaterale-organisaties.git')
+    with cd('/home/projects/multilateral-organisations'):
+        run('git pull git@github.com:openstate/multilateral-organisations.git')
         sudo('docker exec -it mlo_nodejs_1 gulp')
         run('touch uwsgi-touch-reload')
         #sudo('docker exec mlo_nginx_1 nginx -s reload')
