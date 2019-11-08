f64cf2f6c0232fb708a6baf54ef487e8b4bd30c1
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-11-08 18:05:16 +0100

    Update Fabric to v2

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,46 @@
-from fabric.api import run, env, cd, sudo
+from fabric import Connection, Config, task
+from invoke import Exit
+import getpass
 
-env.use_ssh_config = True
-env.hosts = ["Oxygen"]
+# Name of the git repository
+GIT_REPO = 'multilateral-organisations'
+
+# Path of the directory
+DIR = '/home/projects/%s' % (GIT_REPO)
+
+# Container used to compile the assets
+NODE_CONTAINER = 'mlo_nodejs_1'
+
+# Server name
+SERVER = 'Oxygen'
 
 
-def deploy():
-    with cd('/home/projects/multilateral-organisations'):
-        run('git pull git@github.com:openstate/multilateral-organisations.git')
-        sudo('docker exec -it mlo_nodejs_1 gulp')
-        run('touch uwsgi-touch-reload')
-        #sudo('docker exec mlo_nginx_1 nginx -s reload')
+@task
+def deploy(c):
+    sudo_pass = getpass.getpass("Enter your sudo password on %s: " % SERVER)
+    config = Config(overrides={'sudo': {'password': sudo_pass}})
+    c = Connection(SERVER, config=config)
+
+    # Pull from GitHub
+    c.run(
+        'bash -c "cd %s && git pull git@github.com:openstate/%s.git"' % (
+            DIR,
+            GIT_REPO
+        )
+    )
+
+    # Compile assets
+    output = c.sudo(
+        'docker inspect --format="{{.State.Status}}" %s' % (NODE_CONTAINER)
+    )
+    if output.stdout.strip() != 'running':
+        raise Exit(
+            '\n*** ERROR: The %s container, used to compile the assets, is '
+            'not running. Please build/run/start the container.' % (
+                NODE_CONTAINER
+            )
+        )
+    c.sudo('docker exec %s gulp' % (NODE_CONTAINER))
+
+    # Reload app
+    c.run('bash -c "cd %s && touch uwsgi-touch-reload"' % (DIR))
