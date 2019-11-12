d8ff828f88e7c3eb383f3ac4760e5cf3891a9e58
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-11-12 19:27:58 +0100

    Allow for longer vendor names

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Base(db.Model):
     organisation = db.Column(db.String(16), index=True)
     year = db.Column(db.Integer, index=True)
     vendor_country = db.Column(db.String(64), index=True)
-    vendor_name = db.Column(db.String(128), index=True)
+    vendor_name = db.Column(db.String(256), index=True)
     amount = db.Column(db.Numeric(13, 2))
     description = db.Column(db.Text)
     country_code = db.Column(db.String(3), index=True)
